304ac7c1a88b9b346b1b7f18aa235cd484aa1f3b
dysby <>
2023-03-08 18:05:21 +0000

    recover bigbird create

--- src/geo_kpe_multidoc/models/backend/_longmodels.py
+++ src/geo_kpe_multidoc/models/backend/_longmodels.py
@@ -174,8 +174,114 @@ def create_long_model(embedding_model, save_model_to, attention_window, max_pos)
     return callable_model
 
 
-def create_bigbird(model, save_model_to, attention_window, max_pos):
-    pass
+def create_bigbird(
+    model: str, save_model_to: str, attention_window: int, max_pos: int
+) -> Callable:
+    callable_model = select_backend(model)
+
+    model = callable_model.embedding_model._modules["0"]._modules["auto_model"]
+    tokenizer = callable_model.embedding_model.tokenizer
+    config = model.config
+
+    # TODO: Check this
+    tokenizer.model_max_length = max_pos
+    tokenizer.init_kwargs["model_max_length"] = max_pos
+    tokenizer._tokenizer.truncation["max_length"] = attention_window
+
+    current_max_pos, embed_size = model.embeddings.position_embeddings.weight.shape
+    max_pos += 2  # NOTE: RoBERTa has positions 0,1 reserved, so embedding size is max position + 2
+    config.max_position_embeddings = max_pos
+
+    assert max_pos > current_max_pos
+    # allocate a larger position embedding matrix
+    new_pos_embed = model.embeddings.position_embeddings.weight.new_empty(
+        max_pos, embed_size
+    )
+    # copy position embeddings over and over to initialize the new position embeddings
+
+    k = 2
+    step = current_max_pos - 2
+    while k < max_pos - 1:
+        new_pos_embed[k : (k + step)] = model.embeddings.position_embeddings.weight[2:]
+        k += step
+
+    model.embeddings.position_embeddings.weight.data = new_pos_embed
+    model.embeddings.position_ids.data = torch.tensor(
+        [i for i in range(max_pos)]
+    ).reshape(1, max_pos)
+
+    config.attention_window = [attention_window] * config.num_hidden_layers
+    roberta_layers = [layer for _, layer in enumerate(model.encoder.layer)]
+
+    big_bird_config = BigBirdConfig()
+    big_bird_config.update(config.to_dict())
+    big_bird_model = BigBirdModel(big_bird_config)
+    big_bird_layers = [layer for _, layer in enumerate(big_bird_model.encoder.layer)]
+
+    for layer, big_bird_layer in zip(roberta_layers, big_bird_layers):
+        big_bird_layer.attention.self.query = layer.attention.self.query
+        big_bird_layer.attention.self.key = layer.attention.self.key
+        big_bird_layer.attention.self.value = layer.attention.self.value
+        layer.attention.self = big_bird_layer.attention.self
+
+    if not os.path.exists(save_model_to):
+        os.makedirs(save_model_to)
+
+    model.save_pretrained(save_model_to)
+    tokenizer.save_pretrained(save_model_to)
+    return callable_model
+
+
+# def create_bigbird(model : str, save_model_to : str, attention_window : int, max_pos : int ) -> Callable :
+#    callable_model = select_backend(model)
+#
+#    model = callable_model.embedding_model._modules['0']._modules['auto_model']
+#    tokenizer = callable_model.embedding_model.tokenizer
+#    config = model.config
+#
+#    # TODO: Check this
+#    tokenizer.model_max_length = max_pos
+#    tokenizer.init_kwargs['model_max_length'] = max_pos
+#    tokenizer._tokenizer.truncation["max_length"] = attention_window
+#
+#    current_max_pos, embed_size = model.embeddings.position_embeddings.weight.shape
+#    max_pos += 2  # NOTE: RoBERTa has positions 0,1 reserved, so embedding size is max position + 2
+#    config.max_position_embeddings = max_pos
+#
+#    assert max_pos > current_max_pos
+#    # allocate a larger position embedding matrix
+#    new_pos_embed = model.embeddings.position_embeddings.weight.new_empty(max_pos, embed_size)
+#    # copy position embeddings over and over to initialize the new position embeddings
+#
+#    k = 2
+#    step = current_max_pos - 2
+#    while k < max_pos - 1:
+#        new_pos_embed[k:(k + step)] = model.embeddings.position_embeddings.weight[2:]
+#        k += step
+#
+#    model.embeddings.position_embeddings.weight.data = new_pos_embed
+#    model.embeddings.position_ids.data = torch.tensor([i for i in range(max_pos)]).reshape(1, max_pos)
+#
+#    config.attention_window = [attention_window] * config.num_hidden_layers
+#    roberta_layers = [ layer for _, layer in enumerate(model.encoder.layer)]
+#
+#    big_bird_config = BigBirdConfig()
+#    big_bird_config.update(config.to_dict())
+#    big_bird_model = BigBirdModel(big_bird_config)
+#    big_bird_layers = [ layer for _, layer in enumerate(big_bird_model.encoder.layer)]
+#
+#    for layer, big_bird_layer in zip(roberta_layers, big_bird_layers):
+#        big_bird_layer.attention.self.query = layer.attention.self.query
+#        big_bird_layer.attention.self.key = layer.attention.self.key
+#        big_bird_layer.attention.self.value = layer.attention.self.value
+#        layer.attention.self = big_bird_layer.attention.self
+#
+#    if not os.path.exists(save_model_to):
+#        os.makedirs(save_model_to)
+#
+#    model.save_pretrained(save_model_to)
+#    tokenizer.save_pretrained(save_model_to)
+#    return callable_model
 
 
 def load_longmodel(embedding_model: str = "") -> Callable:
