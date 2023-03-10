bc6a30b27d16c03fd4f8d1aa92eca5c1e5ccde4b
dysby <>
2023-03-10 18:13:14 +0000

    process mordecai wip

--- src/geo_kpe_multidoc/datasets/process_mordecai.py
+++ src/geo_kpe_multidoc/datasets/process_mordecai.py
@@ -1,7 +1,10 @@
 import json
 from os import path
 
-import plotly.express as px
+from elasticsearch.exceptions import ConnectionTimeout
+from loguru import logger
+
+# import plotly.express as px
 from mordecai import Geoparser
 
 from geo_kpe_multidoc import (
@@ -12,30 +15,78 @@ from geo_kpe_multidoc import (
 
 
 def process_MKDUC01():
-    parser = Geoparser(es_hosts=GEO_KPE_MULTIDOC_MORDECAI_ES_URL)
+    parser = Geoparser(es_hosts=GEO_KPE_MULTIDOC_MORDECAI_ES_URL, progress=False)
 
-    with open(
-        path.join(GEO_KPE_MULTIDOC_DATA_PATH, "MKDUC-01/MKDUC01.json"), "r"
-    ) as s_json:
+    with open(path.join(GEO_KPE_MULTIDOC_DATA_PATH, "MKDUC01/MKDUC01.json")) as s_json:
         docs = json.load(s_json)
-        res = {}
 
         for doc_group in docs:
-            res[doc_group] = {}
-            for doc in docs[doc_group]["documents"]:
-                res[doc_group][doc] = str(
-                    parser.geoparse(docs[doc_group]["documents"][doc])
-                )
+            try:
+                logger.info(f"Geoparsing group {doc_group}")
+                if doc_group in [
+                    "d08",
+                    "d14",
+                    "d22",
+                    "d28",
+                    "d50",
+                    "d53",
+                ]:
+                    continue
 
-    with open(
-        path.join(GEO_KPE_MULTIDOC_DATA_PATH, "MKDUC-01/MKDUC01-mordecai.json"), "w"
-    ) as d_json:
-        json.dump(res, d_json, indent=4, separators=(",", ": "))
+                res = {
+                    doc_group: {
+                        doc_id: str(parser.geoparse(text))
+                        for doc_id, text in docs[doc_group]["documents"].items()
+                    }
+                }
+
+                with open(
+                    path.join(
+                        GEO_KPE_MULTIDOC_DATA_PATH,
+                        f"MKDUC01/MKDUC01-mordecai-{doc_group}.json",
+                    ),
+                    mode="w",
+                ) as f:
+                    json.dump(res, f, indent=4, separators=(",", ": "))
+                del res
+
+            except ConnectionTimeout as e:
+                logger.error(e)
+                with open(
+                    path.join(
+                        GEO_KPE_MULTIDOC_DATA_PATH,
+                        "MKDUC01/MKDUC01-mordecai-errors.txt",
+                    ),
+                    mode="a",
+                ) as f:
+                    f.write("\n")
+                    f.write(doc_group)
+
+        # assert res_old == res
+
+        # res = {
+        #         topic: {
+        #             doc_id: str(parser.geoparse(text))
+        #             for doc_id, text in topic_docs["documents"].items()
+        #         }
+        #         for topic, topic_docs in docs.items()
+        #     }
+        # with open(
+        #     path.join(GEO_KPE_MULTIDOC_DATA_PATH, "MKDUC01/MKDUC01-mordecai.json"),
+        #     mode="w",
+        # ) as f:
+        #     json.dump(res, f, indent=4, separators=(",", ": "))
+
+        # with open(
+        #     path.join(GEO_KPE_MULTIDOC_DATA_PATH, "MKDUC01/MKDUC01-mordecai-old-3.json"),
+        #     mode="w",
+        # ) as f:
+        #     json.dump(res_old, f, indent=4, separators=(",", ": "))
 
 
 def build_map():
     with open(
-        path.join(GEO_KPE_MULTIDOC_DATA_PATH, "MKDUC-01/MKDUC01-mordecai.json"), "r"
+        path.join(GEO_KPE_MULTIDOC_DATA_PATH, "MKDUC01/MKDUC01-mordecai.json"), "r"
     ) as s_json:
         source = json.load(s_json)
         res = {"geo_loc": [], "country_loc": [], "full_p": []}
@@ -65,9 +116,7 @@ def build_map():
                         res["country_loc"].append(entry["country_predicted"])
 
         with open(
-            path.join(
-                GEO_KPE_MULTIDOC_DATA_PATH, "MKDUC-01/MKDUC01-geo_locations.json"
-            ),
+            path.join(GEO_KPE_MULTIDOC_DATA_PATH, "MKDUC01/MKDUC01-geo_locations.json"),
             "w",
         ) as d_json:
             json.dump(res, d_json, indent=4, separators=(",", ": "))
